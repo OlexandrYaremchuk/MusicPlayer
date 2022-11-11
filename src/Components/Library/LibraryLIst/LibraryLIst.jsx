05d60e7ccfaf6fea92d47703e2679f6ad782511e
OlexandrYaremchuk <70215770+OlexandrYaremchuk@users.noreply.github.com>
2022-11-11 21:21:59 +0200

    add hooks

--- src/Components/Library/LibraryLIst/LibraryLIst.jsx
+++ src/Components/Library/LibraryLIst/LibraryLIst.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import style from './LibraryLIst.module.css'
+import sprite from '../../../image/sprite.svg'
+import { Link } from 'react-router-dom'
 
 
 const LibraryList = (props) => {
+
     return (
 
         <div className="">
+
             <ul className={style.LibraryList}>
-                <li>
-                    <img src={props.imgUrl} alt="" />
-                </li>
-                <li className={style.LibraryText}>
-                    {props.text}
-                </li>
+                <div className={style.listBox}>
+                    <li>
+                        <img src={props.imgUrl} alt="" />
+                    </li>
+                    <li className={style.LibraryText}>
+                        {props.text}
+                    </li>
+                </div>
+
+
             </ul>
 
 
---
+++ src/Components/Music/NewComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function React(props) {
+
+    const [state, setState] = useState('')
+    return (
+        <div className="Main">
+            It Step
+        </div>
+    )
+}
+export default React
